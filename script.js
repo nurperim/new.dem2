4a512306159ef107d6cc4bd5e5ed290f66c3db3e
nurperim <knurperim09@gmail.com>
2026-07-13 00:05:37 +0600

    Simplify form submission alert and remove redundancy
    Removed duplicate submit event listener and simplified alert message.

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const form = document.querySelector("form");
 
+form.addEventListener("submit", function(){
 
-form.addEventListener("submit", function(event) {
-
-    event.preventDefault();
-
-
-    alert("✅ Ваше обращение отправлено. Спасибо, что доверяете New.Dem. Мы свяжемся с вами при необходимости.");
-
-
-    form.reset();
+alert("✅ Ваше обращение отправлено. Спасибо за доверие New.Dem.");
 
 });
